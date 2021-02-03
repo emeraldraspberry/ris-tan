a9633f7d5932884ad303a69a1b076c3c93a5ca6f
emeraldraspberry <48506213+emeraldraspberry@users.noreply.github.com>
2021-02-02 21:00:54 -0600

    Add initial userscript

---
+++ ris-tan.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         ris-tan
+// @namespace    https://github.com/emeraldraspberry/ris-tan
+// @version      0.1
+// @description  Companion userscript that aids with reverse image search on Reddit.
+// @author       EmeraldRaspberry
+// @match        https://*.reddit.com/*/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+})();
